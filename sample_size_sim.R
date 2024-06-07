2ec8df63acfe2896c2aed34d8e62c45da9cfa21b
YupengChen1137 <yupeng000611@gmail.com>
2024-06-06 18:55:25 -0700

    Update sample_size_sim.R

--- sample_size_sim.R
+++ sample_size_sim.R
@@ -12,9 +12,7 @@ library(pwr)
 # Set parameters
 Time <- 210  # Total number of decision points (42 days * 5 times per day)
 rho <- 0.4  # Randomization probability
-availability <- 0.5  # Constant availability
-
-
+availability <- 1  # Constant availability
 
 create_z <- function(t) {
   Z <- matrix(0, nrow = 3, ncol = 1)
@@ -29,8 +27,11 @@ for (t in 1:Time) {
 }
 Q <- t(Z) %*% diag(rep(availability,Time) * rep(rho, Time) * rep((1-rho), Time)) %*% Z
 
+
+
 # Standardized treatment effect (d)
 d <- c(0, 0.00964, -0.000172)
+d <- c(0, 0.00476, 0)
 # d <- c(0, 0.01, -0.0002)
 # beta_t <- matrix(rep(0.1, 210), ncol = 1)
 # d <- solve(t(Z) %*% diag(rep(availability,Time)) %*% Z) %*% t(Z)  %*% diag(rep(availability,Time)) %*% beta_t
@@ -65,10 +66,10 @@ calculate_sample_size <- function(beta, alpha, d, Q) {
     
     # Calculate the critical value for the F-distribution
     inv.f <- qf(1-alpha,df1,df2)
-    inv.f <- qf((N-p-q) *(1-alpha) / p / (N-q-1),df1,df2)
+    #inv.f <- qf((N-p-q) *(1-alpha) / p / (N-q-1),df1,df2)
     # Evaluate the left-hand side of the formula
     calc_power = 1-pf(inv.f,df1,df2,ncp = c_N)
-    calc_power = 1 - p * (N-q-1) * pf(inv.f,df1,df2,ncp = c_N) / (N-p-q)
+    #calc_power = 1 - p * (N-q-1) * pf(inv.f,df1,df2,ncp = c_N) / (N-p-q)
     #calc_power = 1 - pf( p * (N-q-1) * inv.f / (N-p-q),df1,df2,ncp = c_N)
     #print(paste0("Sample size: ", N, ", power: ", calc_power))
     return(calc_power - 0.8)
@@ -91,4 +92,29 @@ alpha <- 0.05
 
 # Calculate sample size
 sample_size <- calculate_sample_size(beta, alpha, d, Q)
- print(paste("Required sample size: ", sample_size))
+print(paste("Required sample size: ", sample_size))
+ 
+
+library(ggplot2)
+sample_size <- 19
+# Define parameters
+df1 <- 3   # degrees of freedom 1
+df2 <- sample_size - 3 -3   # degrees of freedom 2
+c_N <- sample_size * t(d) %*% Q %*% d    # non-centrality parameter
+
+# Define the range for x
+x <- seq(0, 80, length.out = 1000)
+
+# Calculate the pdf values for the F-distribution
+pdf_values <- df(x, df1, df2, c_N) * 3 * (sample_size - 3 -1)  / (sample_size -3-3)
+
+# Create a data frame for plotting
+data <- data.frame(x = x, pdf = pdf_values)
+
+# Plot the pdf
+ggplot(data, aes(x = x, y = pdf)) +
+  geom_line(color = "blue") +
+  labs(title = "PDF of the F-Distribution",
+       x = "x",
+       y = "Density") +
+  theme_minimal()
